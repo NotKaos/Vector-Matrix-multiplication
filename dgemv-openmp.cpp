49fee4b46a1e0868a36e08e6f17627ce1ba1cf60
NotKaos <jacoblau117@gmail.com>
2023-04-24 13:11:27 -0700

    added coded to vectorized and openmp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -18,9 +18,49 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    {
       int nthreads = omp_get_num_threads();
       int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   
+   
    }
+#pragma omp parallel for
+for (int i = 0; i < n; i++) {
+double temp = 0.0;
+for (int j = 0; j < n; j++) {
+temp += A[i*n+j] * x[j];
+}
+#pragma omp atomic
+y[i] += temp;
+}
+}
 
+int main(int argc, char **argv) {
+
+// matrix dimension
+int n = 10000;
+
+// allocate memory for matrix and vectors
+double A = (double)malloc(n * n * sizeof(double));
+double x = (double)malloc(n * sizeof(double));
+double y = (double)malloc(n * sizeof(double));
+
+// initialize matrix and vectors
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) {
+A[i*n+j] = rand() / (double)RAND_MAX;
+}
+x[i] = rand() / (double)RAND_MAX;
+y[i] = rand() / (double)RAND_MAX;
+}
+
+// perform dgemv operation
+my_dgemv(n, A, x, y);
+
+// deallocate memory
+free(A);
+free(x);
+free(y);
+
+return 0;
+}
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,3 +1,4 @@
+#include <immintrin.h>
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
@@ -8,5 +9,12 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
+    __m256d y_vec = _mm256_load_pd(y);
+    for (int i = 0; i < n; i++) {
+        __m256d a_vec = _mm256_load_pd(&A[i*n]); // load a row of A
+        __m256d x_vec = _mm256_broadcast_sd(&x[i]); // broadcast x[i] to a vector
+        y_vec = _mm256_fmadd_pd(a_vec, x_vec, y_vec); // multiply-add a_vec and x_vec to y_vec
+    }
+    _mm256_store_pd(y, y_vec);
 
 }
